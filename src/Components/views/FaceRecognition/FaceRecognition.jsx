eb2e4d38f3a5a59227b6e5efe5f2a95805a9bbcd
Nav <navdeep.dhamrait@thebioagency.com>
2023-10-16 11:25:45 -0400

    added classification and face detection

---
+++ src/Components/views/FaceRecognition/FaceRecognition.jsx
@@ -0,0 +1,146 @@
+import React, { useEffect, useState } from 'react';
+import { storage } from '../../../Firebase';
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
+import Button from '../../Atoms/Button/Button';
+import { useNavigate } from 'react-router-dom';
+
+const FaceRecognition = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [imageURL, setImageURL] = useState([]);
+    const [uploaded, setUploaded] = useState(false);
+    const [faceRecognized, setFaceRecognized] = useState(false);
+
+    const onFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+    let imageSelected = imageURL[0];
+
+    const onFileUpload = () => {
+        //  upload file to firebase storage
+        if (selectedFile === null) return;
+        const imgRef = ref(storage, `images/ ${selectedFile.name + uuidv4()}`);
+        uploadBytes(imgRef, selectedFile).then((snaphsot) => {
+            getDownloadURL(snaphsot.ref).then((url) => {
+                setImageURL((prevArray) => [...prevArray, url]);
+            });
+        });
+
+        //  send file to face recognition api
+        const options = {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+
+            },
+            body: JSON.stringify({
+                response_as_dict: true,
+                attributes_as_list: false,
+                show_original_response: false,
+                providers: 'amazon',
+                file_url: imageSelected,
+            }),
+        };
+
+        fetch('https://api.edenai.run/v2/image/face_recognition/add_face', options)
+            .then((response) => response.json())
+            .then((response) => console.log(response))
+            .catch((err) => console.error(err));
+
+        setUploaded(true)
+    };
+
+    // get all images from firebase storage
+    useEffect(() => {
+        const listRef = ref(storage, 'images/');
+        listAll(listRef)
+            .then((res) => {
+                res.items.forEach((itemRef) => {
+                    getDownloadURL(itemRef).then((url) => {
+                        setImageURL((prevArray) => [...prevArray, url]);
+                    });
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const FaceRecognition = () => {
+
+        const options = {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+                  
+            },
+            body: JSON.stringify({
+                response_as_dict: true,
+                attributes_as_list: false,
+                show_original_response: false,
+                providers: 'amazon',
+                file_url: imageSelected
+            })
+        };
+
+        fetch('https://api.edenai.run/v2/image/face_recognition/recognize', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+
+        setFaceRecognized(true)
+    }
+
+    const navigate = useNavigate();
+    const handleButtonClick = () => {
+        navigate('/ImageClassification')
+    };
+
+    return (
+        <>
+            <div style={{ margin: '30px', display: 'flex', justifyContent: 'center' }}>
+                <input type='file' onChange={onFileChange} />
+                <Button
+                    text='Upload'
+                    onClick={onFileUpload}
+                    backgroundColor={'blue'}
+                />
+            </div>
+            <h1 style={{ textAlign: 'center' }}>List of Recognized Images</h1>
+            {imageURL.map((url, idx) => {
+                return (
+                    <img
+                        key={idx}
+                        src={url}
+                        alt='face'
+                        style={{ width: '220px', height: '220px', margin: '30px', objectFit: 'cover' }}
+                    />
+                );
+            })}
+            {uploaded && <div style={{ marginLeft: '30px' }}>
+                <Button
+                    text='Face Recognition'
+                    onClick={FaceRecognition}
+                    marginTop={'50px'}
+                    marginLeft={'10px'}
+                    backgroundColor={'blue'}
+                />
+            </div>}
+            {faceRecognized && <Button
+                text='Continue'
+                onClick={handleButtonClick}
+                marginTop={'50px'}
+                marginLeft={'40px'}
+                backgroundColor={'blue'}
+            />}
+
+
+        </>
+    );
+};
+
+export default FaceRecognition;
---
+++ src/Components/views/TextImageGenerator.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { useState } from 'react';
+import Button from '../Atoms/Button/Button';
+export default function TextImageGenerator() {
+    const [inputValue, setInputValue] = useState('');
+    const [Images, setImages] = useState([]);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+    const handleSubmit = () => {
+        const options = {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                 authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+            },
+            body: JSON.stringify({
+                response_as_dict: true,
+                attributes_as_list: false,
+                show_original_response: false,
+                resolution: '512x512',
+                num_images: 1,
+                text: inputValue,
+                providers: 'deepai',
+            }),
+        };
+
+        fetch('https://api.edenai.run/v2/image/generation', options)
+            .then((response) => response.json())
+            .then((response) => {
+                const newImages = [
+                    ...Images,
+                    {
+                        url: response['deepai']['items'][0]['image_resource_url'],
+                    },
+                ];
+
+                setImages(newImages)
+            })
+            .catch((err) => console.error(err));
+    };
+
+    console.log(Images);
+    return (
+        <div>
+            <div style={{ margin: '20px', textAlign: 'center' }}>
+                <h1>Text Image generator</h1>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignContent: 'center',
+                    }}
+                >
+                    <textarea
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        rows={5}
+                        cols={40}
+                        placeholder='Enter your text...'
+                    />
+
+                    <Button
+                        text='Submit'
+                        onClick={handleSubmit}
+                        marginTop={'50px'}
+                        marginLeft={'10px'}
+                        backgroundColor={'blue'}
+                    />
+                </div>
+                <h1>AI Genorated Images</h1>
+                <div style={{ display: "grid", gridTemplateColumns: 'repeat(3, 2fr)', gap: '15px' }}>
+                    {Images &&
+                        Images.map((image, idx) => (
+                            <div key={idx}>
+                                <img style={{ width: '100%', height: 'auto' }} src={image.url} alt={image} />
+                            </div>
+                        ))}
+                </div>
+
+            </div>
+        </div>
+    );
+}
